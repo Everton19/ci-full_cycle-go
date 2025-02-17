5b35d162675a3d4c4c6ac68e64f7f18c0a75a5e5
Everton19 <evertontp5@gmail.com>
2025-02-17 16:31:30 -0300

    adicionando função de Soma para testar o ci do github

---
+++ math_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	total := Soma(152, 15)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido: Resultado esperado %d, resultado obtido %d", 30, total)
+	}
+}
